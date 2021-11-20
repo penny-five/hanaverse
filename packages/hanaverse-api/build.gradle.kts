65c6a63631941eb227f1508066ee15a84d2ebdc1
Arttu Hänninen <arttu.hanninen@gofore.com>
2021-11-20 17:26:28 +0200

    Heroku jar manifest

--- packages/hanaverse-api/build.gradle.kts
+++ packages/hanaverse-api/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("java")
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
     kotlin("kapt") version "1.6.0"
@@ -38,4 +39,10 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.hanaverse.HanaverseApplication"
+    }
 }
