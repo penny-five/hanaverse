a19e62605a93dcb4c3c19cb51dac8d63e9b7e8ab
Joonas Lehtonen <joonas.lehtonen@gofore.com>
2021-11-20 15:45:13 +0200

    add new UI stuff

--- packages/hanaverse-client/src/components/pages/Hananoids.js
+++ packages/hanaverse-client/src/components/pages/Hananoids.js
@@ -1,4 +1,5 @@
 import React from "react";
+import HananoidItem from "../HananoidItem";
 
 import "./Hananoids.css";
 
@@ -6,6 +7,15 @@ const HananoidsPage = () => {
   return (
     <div className="hananoidsPage">
       <h1>Hananoids</h1>
+      <ul className="hananoidsList">
+        <HananoidItem name="Bongo" color="green" age={10} />
+        <HananoidItem name="Bongo" color="red" age={10} />
+        <HananoidItem name="Bongo" color="red" age={10} />
+        <HananoidItem name="Bongo" color="green" age={10} />
+        <HananoidItem name="Bongo" color="green" age={10} />
+        <HananoidItem name="Bongo" color="green" age={10} />
+        <HananoidItem name="Bongo" color="green" age={10} />
+      </ul>
     </div>
   );
 };
