14c8bcfef3d3dcd6b3032903d0ee3b6c0fae369e
Esa Niemi <esa.niemi@gofore.com>
2021-11-20 15:26:15 +0200

    Working background

--- packages/hanaverse-client/src/components/Tile.jsx
+++ packages/hanaverse-client/src/components/Tile.jsx
@@ -2,9 +2,9 @@ import { Image } from "react-konva";
 import React from "react";
 
 const Tile = (props) => {
-  const {x, y, width, height, image} = props;
-  // console.log("image", image);
-  return <Image x={x} y={y} width={width} height={height} image={image} />;
+  const { x, y, width, height, image } = props;
+  console.log("image", x, y, width, height, image);
+  return <Image x={x * width} y={y * height} width={width} height={height} image={image} />;
 };
 
 export default Tile;
