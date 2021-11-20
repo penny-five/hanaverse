e88f5a125df0dc3271b788c55071fb4deffd7e92
Joonas Lehtonen <joonas.lehtonen@gofore.com>
2021-11-20 23:16:05 +0200

    improve README.md, cleaning up

--- packages/hanaverse-client/src/api/index.js
+++ packages/hanaverse-client/src/api/index.js
@@ -1,54 +1,5 @@
 import axios from "axios";
 
-const MOCK_RESPONSE = {
-  householdSize: 3,
-  villageName: "Hanaville",
-  hananoids: [
-    {
-      name: "Kwork",
-      color: "GREEN",
-      age: 8,
-    },
-    {
-      name: "Zark",
-      color: "RED",
-      age: 13,
-    },
-  ],
-  hananoidHappiness: 0.2,
-  weatherForecast: "BURNING_HOT",
-  waterConsumptionHistory: [
-    {
-      date: "2020-01-01",
-      liters: 543,
-    },
-    {
-      date: "2020-01-02",
-      liters: 123,
-    },
-    {
-      date: "2020-01-03",
-      liters: 231,
-    },
-    {
-      date: "2020-01-04",
-      liters: 234,
-    },
-    {
-      date: "2020-01-05",
-      liters: 234,
-    },
-    {
-      date: "2020-01-06",
-      liters: 121,
-    },
-    {
-      date: "2020-01-07",
-      liters: 180,
-    },
-  ],
-};
-
 const client = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
