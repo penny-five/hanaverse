042d366b05ca517ed50ba8d9d85a6db285415ff2
Arttu Hänninen <arttu.hanninen@gofore.com>
2021-11-20 17:33:07 +0200

    Heroku jar manifest

--- packages/hanaverse-api/build.gradle.kts
+++ packages/hanaverse-api/build.gradle.kts
@@ -45,4 +45,11 @@ tasks.withType<Jar> {
     manifest {
         attributes["Main-Class"] = "com.hanaverse.HanaverseApplication"
     }
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
