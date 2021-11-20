3ee679244f1e07b3ffb54e8008a81fc89f05d4a2
Joonas Lehtonen <joonas.lehtonen@gofore.com>
2021-11-20 14:42:52 +0200

    add basic client layout

--- packages/hanaverse-client/src/App.js
+++ packages/hanaverse-client/src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
-import GameCanvas from './components/Canvas';
+import "marvel-devices.css/assets/devices.min.css";
+import { useState } from "react";
+
+import "./App.css";
+import BottomNavBar from "./components/BottomNavbar";
+import HananoidsPage from "./components/pages/Hananoids";
+import InfoPage from "./components/pages/Info";
+import StatisticsPage from "./components/pages/Statistics";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("info");
+
+  const onSelectPage = (page) => {
+    console.log(page);
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="App">
-      asd
-      <GameCanvas />
+    <div className="app">
+      <div className="marvel-device iphone8plus silver">
+        <div className="top-bar"></div>
+        <div className="sleep"></div>
+        <div className="volume"></div>
+        <div className="camera"></div>
+        <div className="sensor"></div>
+        <div className="speaker"></div>
+        <div className="screen">
+          <div className="contentWrapper">
+            <div className="canvasWrapper">{/* <GameCanvas /> */}</div>
+            <div className="pageWrapper">
+              {currentPage === "stats" && <StatisticsPage />}
+              {currentPage === "hananoids" && <HananoidsPage />}
+              {currentPage === "info" && <InfoPage />}
+            </div>
+          </div>
+          <div className="navbarWrapper">
+            <BottomNavBar currentPage={currentPage} onSelect={(page) => onSelectPage(page)} />
+          </div>
+        </div>
+        <div className="home"></div>
+        <div className="bottom-bar"></div>
+      </div>
     </div>
   );
 }
---
+++ packages/hanaverse-client/src/components/pages/Info.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./Info.css";
+
+const InfoPage = () => {
+  return (
+    <div className="infoPage">
+      <h1>About Hana Village</h1>
+      <h2>Hananoids</h2>
+      <p>
+        The Hananoids are known for their rain droplet shape, round eyes, and goofy, toothless
+        smile. Hananoids love water and nature that water grows around them, and also art, music and
+        poetry, and all the good things of the world.
+      </p>
+      <h2>Hana Village</h2>
+      <p>
+        This is the town where Hananoids live. Ideally, the Hana Village is surrounded with fertile
+        grass. Plentiful rain throughout the whole year in the village makes it a place full of
+        lakes and rivers. Hananoids are good at swimming and cultivating the land.
+      </p>
+      <h2>Water consumption</h2>
+      <p>
+        Since water is essential for the Hananoids and their land to prosper, your responsibility is
+        to conserve water and make sure the Hananoids have enough water for them to grow and stay
+        healthy.
+      </p>
+    </div>
+  );
+};
+
+export default InfoPage;
