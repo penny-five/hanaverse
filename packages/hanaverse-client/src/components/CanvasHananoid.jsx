f9841c29bc7660ef9d614a04b999b092a9f3dade
Esa Niemi <esa.niemi@protonmail.com>
2021-11-20 19:54:50 +0200

    Animated blue noids

--- packages/hanaverse-client/src/components/CanvasHananoid.jsx
+++ packages/hanaverse-client/src/components/CanvasHananoid.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Image } from "react-konva";
+import { useRef, useState, useEffect } from "react";
+import { Image, Sprite } from "react-konva";
 
 const HANANOID_COLORS = ["blue", "green", "orange", "red"];
 
@@ -8,17 +8,33 @@ const HANANOID_MOODS = ["happy", "sad"];
 const CanvasHananoid = (props) => {
   const [x, setX] = useState(Math.round(Math.random() * 13) * 32);
   const [y, setY] = useState(Math.round(Math.random() * 13) * 32);
+  const spriteRef = useRef(null);
 
   const resolveImage = () => {
-    const color = HANANOID_COLORS[Math.round(Math.random() * (HANANOID_COLORS.length - 1))];
-    const mood = HANANOID_MOODS[Math.round(Math.random() * (HANANOID_MOODS.length - 1))];
-
     let image = new window.Image();
-    image.src = `${process.env.PUBLIC_URL + `/hananoids/${color}-${mood}.png`}`;
+    image.src = `${process.env.PUBLIC_URL + "/hananoids/blue-happy.png"}`;
     return image;
   };
 
-  return <Image x={x} y={y} image={resolveImage()} />; // TODO change into an animated sprite
+  useEffect(() => {
+    spriteRef.current.start();
+  }, [spriteRef]);
+
+  return (
+    <Sprite
+      ref={spriteRef}
+      x={x}
+      y={y}
+      image={resolveImage()}
+      animation="bouncing"
+      animations={{
+        // x, y, width, height (4 frames)
+        bouncing: [0, 0, 32, 44, 32, 0, 32, 44, 64, 0, 32, 44],
+      }}
+      frameRate={2}
+      frameIndex={0}
+    />
+  );
 };
 
 export default CanvasHananoid;
--- packages/hanaverse-client/src/components/HananoidCanvasSprites.jsx
+++ packages/hanaverse-client/src/components/HananoidCanvasSprites.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { Image, Sprite } from "react-konva";
 import CanvasHananoid from "./CanvasHananoid";
 
 const HananoidCanvasSprites = (props) => {
   const { count } = props;
-
-  const positions = [];
-
   const hananoids = Array(count)
     .fill()
     .map(() => {
       return <CanvasHananoid />;
     });
-
-  console.log(hananoids);
 
   return hananoids;
 };
